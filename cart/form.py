90bece1434ba7f32fdd48bf018eb16daf28f7a0d
sylpi <syl.pillet@hotmail.fr>
2020-03-21 03:25:31 +0100

    update add to cart

--- cart/form.py
+++ cart/form.py
@@ -1,11 +1,11 @@
 from django import forms
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 12)]
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 13)]
 
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
-                                choices= PRODUCT_QUANTITY_CHOICES,
+                                choices=PRODUCT_QUANTITY_CHOICES,
                                 coerce=int)
     update = forms.BooleanField(required=False,
                                 initial=False,
