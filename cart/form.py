e781c7be47513961bdff520cbb0d71c97c7a30ea
sylpi <syl.pillet@hotmail.fr>
2020-03-27 19:36:00 +0100

    translate forms (change labels)

--- cart/form.py
+++ cart/form.py
@@ -5,6 +5,7 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 13)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
+                                label="Quantité",
                                 choices=PRODUCT_QUANTITY_CHOICES,
                                 coerce=int)
     update = forms.BooleanField(required=False,
--- orders/form.py
+++ orders/form.py
@@ -3,6 +3,16 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+
     class Meta:
         model = Order
         fields = ["first_name", 'last_name', 'email', 'address', 'postal_code', 'city']
+        labels = {
+            'first_name': 'Votre Prénom',
+            'last_name': 'Votre Nom',
+            'email': 'Votre mail',
+            'address': 'Votre Adresse',
+            'postal_code': 'Code Postal',
+            'city': 'Ville',
+        }
+
