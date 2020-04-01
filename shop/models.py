f55f6a71e242a761e901956e96479979d77369d2
sylpi <syl.pillet@hotmail.fr>
2020-04-02 01:52:14 +0200

    re-rewrite some style and image resize

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,8 @@
-
 from django.db import models
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill, SmartResize
 
 
 class Category(models.Model):
@@ -9,7 +10,7 @@ class Category(models.Model):
                             db_index=True)
     slug = models.SlugField(max_length=200,
                             unique=True)
-    
+
     class Meta:
         ordering = ('name',)
         verbose_name = 'category'
@@ -28,12 +29,13 @@ class Product(models.Model):
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',  blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    smart = ImageSpecField(source='image', processors=[SmartResize(246, 268)])
 
     class Meta:
         ordering = ('name',)
